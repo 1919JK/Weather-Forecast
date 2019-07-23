48089a0e857f6cf3b0426ff51d79fb1ecfb401d2
1919JK <53226234+1919JK@users.noreply.github.com>
2019-07-23 23:51:06 +0900

    Update WeatherForecast.py

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -5,10 +5,11 @@ today = dt.date.today()
 url = "https://www.yomiuri.co.jp/"
 html = rq.get(url)
 page = bs(html.content, "html.parser")
+#pageから天気予報の部分を切り出し
 weather_forecast = page.find("table", attrs = {"class", "home-weather-forecast-list"})
 td = weather_forecast.find_all("td")
 span = weather_forecast.find_all("span")
-
+#↑からリストを取得するための関数。目的のデータは周期的に表れるためmodで選択。
 def get_list (ls_in, freq, mod):
     j = 0
     ls_out = []
@@ -18,10 +19,10 @@ def get_list (ls_in, freq, mod):
         j += 1
     return ls_out
 
-city = get_list(td, 2, 0)
-weather = get_list(span, 3, 0)
-temp_high = get_list(span, 3, 1)
-temp_low = get_list(span, 3, 2)
+city = get_list(td, 2, 0) #リスト：都市
+weather = get_list(span, 3, 0) #リスト：天気
+temp_high = get_list(span, 3, 1) #リスト：最高気温
+temp_low = get_list(span, 3, 2) #リスト：最低気温
 
 tomorrow = today + dt.timedelta(days = 1)
 
